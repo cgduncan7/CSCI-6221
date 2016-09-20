1488f7fe618d5fd2058e637f44e10dd542346de2
Collin Duncan <cgduncan7@gmail.com>
2016-09-19 21:06:14 -0400

    Finished unit tests and added test files.

--- edu.gwu.csci.6221.summer.2016.group19.a2/test/P1Test.java
+++ edu.gwu.csci.6221.summer.2016.group19.a2/test/P1Test.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class P2Test {
+public class P1Test {
 
 	private int[] constructExpectedValues(int size) {
 		int[] expectedValues = new int[size];
@@ -13,62 +13,62 @@ public class P2Test {
 		return expectedValues;
 	}
 	
-	private int[][] P2_testRandomIntArray(int size) {
+	private int[][] P1_testRandomIntArray(int size) {
 		
 		int[] expectedValues = constructExpectedValues(size);
 		
-		P2 p2 = new P2("" + size, false);
-		int[] outputValues = p2.sort(p2.getNums());
+		P1 p1 = new P1("" + size, false);
+		int[] outputValues = p1.sort(p1.getNums());
 		
 		return new int[][]{expectedValues, outputValues};
 	}
 	
 	@Test
-	public void P2_testRandomIntArray_Small() {
-		int[][] vals = P2_testRandomIntArray(50);
+	public void P1_testRandomIntArray_Small() {
+		int[][] vals = P1_testRandomIntArray(50);
 		
 		assertArrayEquals("Output values do not match expected values.", 
 				vals[0], vals[1]);
 	}
 	
 	@Test
-	public void P2_testRandomIntArray_Medium() {
-		int[][] vals = P2_testRandomIntArray(1000);
+	public void P1_testRandomIntArray_Medium() {
+		int[][] vals = P1_testRandomIntArray(1000);
 		
 		assertArrayEquals("Output values do not match expected values.", 
 				vals[0], vals[1]);
 	}
 	
 	@Test
-	public void P2_testRandomIntArray_Large() {
-		int[][] vals = P2_testRandomIntArray(100000);
+	public void P1_testRandomIntArray_Large() {
+		int[][] vals = P1_testRandomIntArray(100000);
 		
 		assertArrayEquals("Output values do not match expected values.", 
 				vals[0], vals[1]);
 	}
 	
 	@Test
-	public void P2_testFileInput_Small() {
-		P2 p2 = new P2(System.getProperty("user.dir") + "/test/test_s.txt", false);
-		int[] outputValues = p2.sort(p2.getNums());
+	public void P1_testFileInput_Small() {
+		P1 p1 = new P1(System.getProperty("user.dir") + "/test/test_s.txt", false);
+		int[] outputValues = p1.sort(p1.getNums());
 		int[] expectedValues = constructExpectedValues(outputValues.length);
 		assertArrayEquals("Output values do not match expected values.",
 				expectedValues, outputValues);
 	}
 	
 	@Test
-	public void P2_testFileInput_Medium() {
-		P2 p2 = new P2(System.getProperty("user.dir") + "/test/test_m.txt", false);
-		int[] outputValues = p2.sort(p2.getNums());
+	public void P1_testFileInput_Medium() {
+		P1 p1 = new P1(System.getProperty("user.dir") + "/test/test_m.txt", false);
+		int[] outputValues = p1.sort(p1.getNums());
 		int[] expectedValues = constructExpectedValues(outputValues.length);
 		assertArrayEquals("Output values do not match expected values.",
 				expectedValues, outputValues);
 	}
 	
 	@Test
-	public void P2_testFileInput_Large() {
-		P2 p2 = new P2(System.getProperty("user.dir") + "/test/test_l.txt", false);
-		int[] outputValues = p2.sort(p2.getNums());
+	public void P1_testFileInput_Large() {
+		P1 p1 = new P1(System.getProperty("user.dir") + "/test/test_l.txt", false);
+		int[] outputValues = p1.sort(p1.getNums());
 		int[] expectedValues = constructExpectedValues(outputValues.length);
 		assertArrayEquals("Output values do not match expected values.",
 				expectedValues, outputValues);
